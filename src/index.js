77b55bd247de4d8f89d26cda02601aef0f78bfd1
YaAnastasia <anastaziii19@gmail.com>
2023-09-28 22:16:58 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -15,3 +15,39 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+/*import React from "react";
+import { render } from "react-dom";
+
+import { App } from "./App";
+
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+*/
